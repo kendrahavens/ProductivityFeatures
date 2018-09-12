c24044cdabe1b30978b232f0a3eb1a6b1fea1cb5
kendrahavens <kehavens@microsoft.com>
2018-09-11 20:09:37 -0700

    First code cleanup

--- Productivity-15.8/ShowMeUpdates.cs
+++ Productivity-15.8/ShowMeUpdates.cs
@@ -9,9 +9,9 @@ namespace Productivity15._8
 
         // Code Cleanup
         // Press (Ctrl k, d) to fix the spacing below.
-        public void FixSpacing     (        )
-                    {
-            }
+        public void FixSpacing()
+        {
+        }
 
         // Code Cleanup
         // If you'd like Code cleanup to include var and explicit type rules
@@ -120,7 +120,7 @@ namespace Productivity15._8
         // Type (Ctrl + .) to remove the extra parenthesis.
         public void RemoveParenthesis()
         {
-            if ((true) && !(2>1)) { }
+            if ((true) && !(2 > 1)) { }
         }
 
         // Place your cursor in the for loop statement.
@@ -128,9 +128,9 @@ namespace Productivity15._8
         // Use (Ctrl + .) to convert it back into a for loop.
         public void ForLoopToForEach()
         {
-            List<int> intList = new List<int>() { 1,2,3};
+            List<int> intList = new List<int>() { 1, 2, 3 };
 
-            for(int i = 0; i < intList.Count; i++)
+            for (int i = 0; i < intList.Count; i++)
             {
                 int num = intList[i];
                 Console.WriteLine(intList[num]);
