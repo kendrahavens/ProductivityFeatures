02945aafbcdbbfd72c681042a171c17f6f51d69d
Kendra Havens <kehavens@microsoft.com>
2020-01-24 17:01:03 -0800

    Added more explanations

--- ProductivityFeatures/Update165P2.cs
+++ ProductivityFeatures/Update165P2.cs
@@ -11,7 +11,7 @@ namespace ProductivityFeatures
             var s = "";
 
             // Select all += lines and extract local function
-            // using the lightbulb (ctrl + .)
+            // using the lightbulb (Ctrl + .)
             s += "Hello";
             s += "Extract";
             s += "Local";
@@ -20,38 +20,34 @@ namespace ProductivityFeatures
             Console.WriteLine(s);
         }
 
-        // Make this method static using the lightbulb after 
-        // putting cursor on name. Works in static classes
-        public void MakeStatic()
-        {
-
-        }
-
         public static void SimplifyInterpolation(int i)
         {
             // Interpolated strings have a simplified notation
             // for common cases, including padding. Interpolation
             // simplification is a configurable diagnostic with
             // a code fix.
+            // Hover over suggestions in the interpolated string
+            // and use the lightbulb to apply the refactoring.
             var s = $"... {i.ToString("bar").PadLeft(3)} ...";
             Console.WriteLine(s);
         }
 
         public static void ShowCompletionForUnimportedTypes()
         {
-            // Type "Task" below and show completion.
-            // Feature now enabled by default
-
-            // Also works for extension methods.
+            // Type "Task" below and show completion for unimported types.
+            // This feature (originally added in 16.1) is now enabled by default.
+            
+            // Completion also now works for extension methods.
             // Type double quotes for empty string "" 
             // and try to invoke .All by typing 
-            // ".A", completion should show All from
-            // System.Linq
+            // ".A", completion should show the All 
+            // extension method from System.Linq
+            
         }
 
         public static string ConvertIfToSwitchStatementOrExpression(int i)
         {
-            // Put cursor on if and invoke lightbulb (ctrl + .)
+            // Put cursor on if and invoke lightbulb (Ctrl + .)
             // to convert to switch expression or switch statement
             if (i == 0)
             {
@@ -98,10 +94,25 @@ namespace ProductivityFeatures
                 throw new InvalidOperationException();
             }
         }
-
-        // NuGet packages for refactorings, previously only VSIX 
-
-        // Editorconfig now supports the below for marking generated code
-        // generated_code = true
     }
+
+    public static class MakeStatic
+    {
+        // Uncomment the method below to cause an error.
+        // Make this method static using the lightbulb after 
+        // putting cursor on name. Works in static classes
+        //public void MakeStatic()
+        //{
+
+        //}
+    }
+
+    // Other features in Update 16.5:
+
+    // Users who create their own refactorings with Roslyn can now publish 
+    // those as NuGet packages, previously the could only publish as a VSIX extension.
+
+    // You can now exclude generated code from being included in code style feedback by
+    // adding the below text to your EditorConfig
+    // generated_code = false
 }
